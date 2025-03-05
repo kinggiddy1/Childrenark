4678273c1b6427d82ba7f3949a9ac4f0c20e5f3c
kinggiddy1 <kinggiddy@gmail.com>
2025-03-05 17:28:46 +0200

    the finished project of children's ark

--- index.php
+++ index.php
@@ -121,7 +121,7 @@
     </section>
 
     <section id="clients" class="clients section-bg">
-      <div class="container">
+      <div class="container"> 
 
         <div class="row">
           <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
